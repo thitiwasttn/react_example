381fd928f0d7f65922cc9d6b0cd64832bf7d0c84
GA007007\Admin <thitiwas.n@g-able.com>
2021-11-16 20:49:52 +0700

    up

--- src/components/propfunction/PropFunctionChild.js
+++ src/components/propfunction/PropFunctionChild.js
@@ -1,9 +1,21 @@
 import React from "react";
 
-const PropFunctionChild = () => {
+const PropFunctionChild = (prop) => {
+    let status = prop.data.status;
+    let coupon = prop.data.secretWord;
+    const onCouponUse = event => {
+        prop.setCoupon();
+    };
     return (
         <div>
-
+            <div>
+                รหัสคูปอง: {coupon}
+            </div>
+            <div>
+                <button
+                    onClick={onCouponUse}
+                    disabled={!status}>ใช้คูปอง</button>
+            </div>
         </div>
     );
 }
--- src/components/propfunction/PropFunctionParent.js
+++ src/components/propfunction/PropFunctionParent.js
@@ -1,9 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
+import PropFunctionChild from "./PropFunctionChild";
 
 const PropFunctionParent = () => {
+    const [state, setState] = useState({
+        coupon1: {
+            secretWord: 'fklewkfewofkewpfkewofwkfp',
+            status: true
+        },
+        coupon2: {
+            secretWord: 'keowkdoewkfoewkfoewpkfw',
+            status: true
+        }
+    });
+
+    const useCoupon = (name) => {
+        console.log('useCoupon');
+        const updateCoupon = {...state};
+        console.log(updateCoupon)
+        updateCoupon[name].status = false;
+        setState(updateCoupon);
+    };
     return (
         <div>
-            
+            <PropFunctionChild
+                data={state.coupon1}
+                setCoupon={useCoupon.bind(this, 'coupon1')}
+            />
+
+            <PropFunctionChild
+                data={state.coupon2}
+                setCoupon={useCoupon.bind(this, 'coupon2')}
+            />
         </div>
     );
 }
